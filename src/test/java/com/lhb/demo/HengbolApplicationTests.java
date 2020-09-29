245cebe1d649a291476b90ab794d23fd969aea8e
cdwk <lhb>
2020-09-29 16:06:24 +0800

    1

---
+++ src/test/java/com/lhb/demo/HengbolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lhb.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HengbolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
